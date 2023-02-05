f3e903034de786fc42250c067a5a2e4622eb07f5
NomoteteS <nomotetes.onetrue@icloud.com>
2023-02-05 13:14:15 +0200

    chore: initialize SwiftUI files

---
+++ Crewl/Crewl/CrewlApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrewlApp.swift
+//  Crewl
+//
+//  Created by NomoteteS on 5.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct CrewlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
